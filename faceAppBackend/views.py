c2dd69d85f42755897537bc50ee91f69cc86757c
jatinparate <paratejtn@gmail.com>
2020-01-04 17:12:39 +0530

    Created Login Route and Added Firebase functionality

--- faceAppBackend/views.py
+++ faceAppBackend/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View
 
-# Create your views here.
+import os
+
+import firebase_admin
+from firebase_admin import credentials, db
+
+cred = credentials.Certificate(os.getcwd() + '/creds.json')
+firebase_app = firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://faceapp-db03e.firebaseio.com/'
+})
+root = db.reference()
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class LoginView(View):
+
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("GET")
+
+    def post(self, request: HttpRequest, *args, **kwargs):
+        posted_details = [value for item, value in request.POST.items()]
+        faculties = db.reference('/Faculties/').get()
+        for faculty in faculties:
+            if faculty['email'] == posted_details[0] and faculty['password'] == posted_details[1]:
+                return HttpResponse("Logged In")
+
+        return HttpResponse("Not Logged In")
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/jatin/Desktop/workspace/faceAppBackEnd/venv"
+set -gx VIRTUAL_ENV "/home/jatin/PycharmProjects/faceAppBackEnd/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
